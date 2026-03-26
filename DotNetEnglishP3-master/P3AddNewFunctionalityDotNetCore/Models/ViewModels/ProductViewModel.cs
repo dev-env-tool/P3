df06ded8ed74a3fd28ac92d0cac7d1553f862b2f
260961435+dev-env-tool@users.noreply.github.com <260961435+dev-env-tool@users.noreply.github.com>
2026-03-26 17:15:10 +0100

    Modifications : adding regular expressions for Product creation.

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -9,18 +9,21 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
         [BindNever]
         public int Id { get; set; }
 
-        //[Required]
+        [Required(ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "MissingName")]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
         public string Details { get; set; }
 
-        //[Required]
+        [Required(ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "MissingStock")]
+        [RegularExpression("^((?!^Stock$)[0-9])+$", ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "StockNotAnInteger")]
+        [Range(1, double.MaxValue, ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "StockNotGreaterThanZero")]
         [P3AddNewFunctionalityDotNetCore.Resources.Models.Services.PriceNotDouble]
         public string Stock { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "MissingPrice")]
+        [Range(0.01, 10000000000, ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "PriceNotGreaterThanZero")]
         [P3AddNewFunctionalityDotNetCore.Resources.Models.Services.PriceGreaterThan0]
         [P3AddNewFunctionalityDotNetCore.Resources.Models.Services.PriceNotDouble]
         public string Price { get; set; }
@@ -29,7 +32,6 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
 
     public class GetErrorMessage
     {
-        public string errorMessage { get; set; }
-        public string PpropretyName { get; set; }
+        public string errorMessage = P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService.MissingPrice;
     }
 }
