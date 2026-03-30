83d9fc72ec50149dd5ab0c34fbebe54d8ed2099b
260961435+dev-env-tool@users.noreply.github.com <260961435+dev-env-tool@users.noreply.github.com>
2026-03-30 11:35:53 +0200

    Regex comments and modifications to detect <0 values.

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/ViewModels/ProductViewModel.cs
@@ -17,18 +17,54 @@ namespace P3AddNewFunctionalityDotNetCore.Models.ViewModels
         public string Details { get; set; }
 
         //<summary>
+        //Use of Required to spot on an empty field
         [Required(ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "MissingStock")]
+        //</summary>
+
+        //<summary>
+        //This "regex" should always be sat before the "range" so that it can detect for any <0 values
+        //Use of a regular expression
+        //"^(all which is diferent from)
+        //((?!^Stock$) the field stock, only filled once per field.
+        //-? the string may begin with one "-" character. Only one "-" character is allowed.
+        //[0-9]) a number only composed of digits from 0 to 9 (inclued)
+        //+$" end of the string
         [RegularExpression("^((?!^Stock$)-?[0-9])+$", ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "StockNotAnInteger")]
+        //</summary>
+
+        //<summary>
+        //Use of a "Range" attribute to check wether the field is an integer between 1 and Signed 2 bytes int = 2,147,483,647.
         [Range(1, int.MaxValue, ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "StockNotGreaterThanZero")]
         //</summary>
         
         public string Stock { get; set; }
 
 
-        [Required(ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "MissingPrice")]
-        [Range(0.01, double.MaxValue, ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "PriceNotGreaterThanZero")]
-        [RegularExpression("^((?!^Price$)-?^(\\d+\\.?\\d+|\\d))+$", ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "PriceNotANumber")]
 
+
+        //<summary>
+        //Use of Required to spot on an empty field
+        [Required(ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "MissingPrice")]
+        //</summary>
+
+        //<summary>
+        //Use of a regular expression
+        //This "regex" should always be sat before the "range" so that it can detect for any <0 values
+        //"^(all which is diferent from)
+        //((?!^Stock$) the field stock, only filled once per field.
+        //-? the string may begin with one "-" character. Only one "-" character is allowed.
+        //(\\d+\\.?\\d+|\\d) A number composed of 1 or more digits [0-9]
+        //followed by a "." character which is only allowed once
+        //followed by a number of one or more digits.
+        //+$" end of the string
+        //
+        [RegularExpression("^((?!^Price$)-?(\\d+\\.?\\d+|\\d))+$", ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "PriceNotANumber")]
+        //</summary>
+
+        //<summary>
+        //Use of a "Range" attribute to check wether the field is a double between 1 and signed 4 bytes double = 1.7976931348623157*E+308.
+        [Range(0.01, double.MaxValue, ErrorMessageResourceType = typeof(P3AddNewFunctionalityDotNetCore.Resources.Models.Services.ProductService), ErrorMessageResourceName = "PriceNotGreaterThanZero")]
+        //</summary>
         public string Price { get; set; }
     }
 
